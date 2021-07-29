cc8680d17abaa80c87b881b15e0b7e10ffd4e30c
Daniel Angelus <danielangelus@yahoo.com.br>
2021-07-29 01:21:24 -0300

    commit

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export {default as Home} from './Home'
+export {default as Details} from './Details'
+export {default as Settings} from './Settings'
+export {default as Perfil} from './Perfil'
+export {default as Screen} from './Screen'
+export {default as Notification} from './Notification'
